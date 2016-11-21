f85de67dda0d27499882cd5ba51d20f70431192e
Péter Hauszknecht <peter.hauszknecht@jaystack.com>
2016-11-21 15:55:37 +0100

    fix mongo connection

--- src/mongo/connection.ts
+++ src/mongo/connection.ts
@@ -1,5 +1,5 @@
 import { MongoClient, Db } from "mongodb";
 
 export default async function():Promise<Db>{
-    return await MongoClient.connect("mongodb://localhost:27017/odata-v4-server-example");
+    return await MongoClient.connect("mongodb://127.0.0.1:27017/odata-v4-server-example");
 };
