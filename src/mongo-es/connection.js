20ffbd6a16828bee71008dbbf33aefaecf3643e3
Péter Hauszknecht <hasyee@gmail.com>
2016-11-25 11:16:52 +0100

    prepare for separating

--- src/mongo-es/connection.js
+++ src/mongo-es/connection.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export default async function(){
     const uri = process.env.NODE_ENV === "production" ?
-        "mongodb://mongo/odata-v4-server-example" :
-        "mongodb://localhost:27017/odata-v4-server-example";
+        "mongodb://mongo/northwind" :
+        "mongodb://localhost:27017/northwind";
     return await MongoClient.connect(uri);
 };
--- src/mongo/connection.ts
+++ src/mongo/connection.ts
@@ -2,7 +2,7 @@ import { MongoClient, Db } from "mongodb";
 
 export default async function():Promise<Db>{
     const uri = process.env.NODE_ENV === "production" ?
-        "mongodb://mongo/odata-v4-server-example" :
-        "mongodb://localhost:27017/odata-v4-server-example";
+        "mongodb://mongo/northwind" :
+        "mongodb://localhost:27017/northwind";
     return await MongoClient.connect(uri);
 };
