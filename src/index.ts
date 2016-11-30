1c75a61e113a398ddd7baf3e27d57b025380a53d
Péter Hauszknecht <peter.hauszknecht@jaystack.com>
2016-11-30 16:27:25 +0100

    add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import * as path from "path";
 import * as express from "express";
+import * as cors from "cors";
+
 import api from "./ts";
 
 const app = express();
 
+app.use(cors());
 app.use("/api", api);
 app.use(express.static('public'))
 
